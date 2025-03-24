0cb41fcb1466cdacab0823cb0a116deb14962b56
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2025-03-24 10:27:58 -0300

    Create script.js

---
+++ Aula_24_03_2025/script.js
@@ -0,0 +1,47 @@
+const precoGasolina = 5.79;
+const precoEtanol = 4.29;
+const precoDiesel = 6.19;
+
+
+function atualizarValor(){
+    const tipo = document.getElementById("combustivel").value;
+    const litros = parseFloat(document.getElementById("litros").value);
+    let precoPorLitro;
+
+    switch (tipo) {
+        case "gasolina":
+            precoPorLitro = precoGasolina;
+            break;
+        case "etanol":
+            precoPorLitro = precoEtanol;
+            break;
+        case "diesel":
+            precoPorLitro = precoDiesel;
+            break;
+        default:
+            document.getElementById("resultado").textContent = "Inválido";
+            return;
+    }
+    calcularValorAbastecimento(precoPorLitro, litros);
+}
+
+function calcularValorAbastecimento(precoCombustivel, litros) {
+    if(litros <=0 || isNaN(litros)){
+        document.getElementById("resultado").textContent = "Inválido";
+        return;
+    }
+    
+    const valorTotal = precoCombustivel * litros;
+    document.getElementById("resultado").textContent = valorTotal;
+}
+
+
+document.getElementById("litros").addEventListener("input", atualizarValor);
+document.getElementById("combustivel").addEventListener("change", atualizarValor);
+
+document.getElementById("litros").addEventListener("keydown", function(event){
+    if (event.key === "Enter"){
+        event.preventDefault();
+        atualizarValor();
+    }
+})
